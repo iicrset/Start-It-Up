21f879e028081e660af487ebce3356dfe607b692
Jeslyn Mariam <jeslynmariam@gmail.com>
2025-11-07 20:05:38 +0530

    speakers

--- src/Data/mentors.js
+++ src/Data/mentors.js
@@ -1,27 +1,8 @@
-import AndreaOliva from '../Assets/Andrea_Oliva.jpeg';
+
+import Vinitha from '../Assets/Vinitha.jpg';
 import Brajesh from '../Assets/Brajesh_Kaimal.jpg';
-import AnirudhDayanand from '../Assets/Anirudh_Dayanand.jpeg';
-import BibsonJoseph from '../Assets/Bibson_Joseph.jpeg';
-import FahadPN from '../Assets/Fahad_P_N.jpeg';
-import JacobRamos from '../Assets/Jacob_Ramos.jpeg';
-import JofinJoseph from '../Assets/Jofin_Joseph.jpeg';
-import JoshaneKelsy from '../Assets/Joshane_Kelsy.jpeg';
-import MilanPSony from '../Assets/Milan_P_Sony.jpeg';
-import NikhilKilivayil from '../Assets/Nikhil_Kilivayil.jpeg';
-import PratikPrakash from '../Assets/Pratik_Prakash.jpeg';
-import CyriacTMeppuram from '../Assets/cyriac_t_meppuram.jpeg';
-import NitheeshKurian from '../Assets/Nitheesh_Kuriyan.jpeg';
-import SaurabhWani from '../Assets/Saurabh_Wani.jpeg';
-import AnvarTK from '../Assets/Anvar_T_K.jpg';
-import EricVilla from '../Assets/Eric_Villa.jpg';
-import YasirM from '../Assets/Yasir_M.jpg';
-import VinodBalan from '../Assets/Vinod_Balan.jpg';
-import CSManuFrancis from '../Assets/C_S_Manu_Francis.jpg';
-import AnandSukumaran from '../Assets/Anand_Sukumaran.jpg';
-import RabinBaby from '../Assets/Rabin_Baby.jpg';
-import JudeMathew from '../Assets/Jude_Mathew.jpg';
-import PiyushAggarwal from '../Assets/Piyush_Aggarwal.jpg';
-import FelixJosemon from '../Assets/Felix_Josemon.jpg';
+import pinky from '../Assets/pinky.jpg';
+import Rahul_raghav from '../Assets/Rahul_raghav.jpg';
 export const mentors = [
     {
         name: 'Brajesh C. Kaimal PMP',
@@ -30,151 +11,29 @@ export const mentors = [
         organization: 'Malabar Angel Network',
         description: 'Journey of a Founder'
     },
+   
     {
-        name: 'Joshane Kelsy',
-        image: JoshaneKelsy,
-        designation: 'Group Product Manager',
-        organization: 'Rakuten',
-        description: 'Mentor'
+        name: 'Vinitha Joseph',
+        image: Vinitha,
+        designation: 'Senior Technology Fellow, Leadership & Life coach, EQ trainer, L&D Facilitator, Speaker',
+        organization: 'Kerala Startup Mission',
+        description: 'Human Sides of Entrepreneurship'
+    },
+   
+    {
+        name: 'Pinky Jayaprakash ',
+        image: pinky,
+        designation: 'Head of People & Culture',
+        organization: 'Moolya',
+        description: 'Lean Startups'
     },
     {
-        name: 'Bibson Joseph',
-        image: BibsonJoseph,
-        designation: 'Data Scientist',
-        organization: 'The World Bank',
-        description: 'Speaker on "Building an MVP with limited resources"'
+        name: 'Rahul Raghav',
+        image: Rahul_raghav,
+        designation: 'Co-Founder, Business Operations Architect, Mentor',
+        organization: 'TWT Venture Studio',
+        description: 'Design Thinking and Problem Validation'
     },
-    {
-        name: 'Pratik Prakash',
-        image: PratikPrakash,
-        designation: 'Founding Member',
-        organization: 'Mailmodo (Y Combinator \'21)',
-        description: 'Mentor'
-    },
-    {
-        name: 'Jofin Joseph',
-        image: JofinJoseph,
-        designation: 'Partner',
-        organization: 'Pienza',
-        description: 'Speaker on "Approaching Angel Investors & VCs"'
-    },
-    {
-        name: 'Anand Sukumaran',
-        image: AnandSukumaran,
-        designation: 'Co-Founder & CTO',
-        organization: 'Engagespot (Techstars NYC)',
-        description: 'Mentor'
-    },
-    {
-        name: 'Cyriac T Meppuram',
-        image: CyriacTMeppuram,
-        designation: 'Founder',
-        organization: 'The Wellness Junxion',
-        description: 'Mentor'
-    },
-    {
-        name: 'Anirudh Dayanand',
-        image: AnirudhDayanand,
-        designation: 'Co-Founder',
-        organization: 'SoftServedWeb',
-        description: 'Speaker at "MVP Sprint"'
-    },
-    {
-        name: 'Fahad P N',
-        image: FahadPN,
-        designation: 'Co-Founder',
-        organization: 'SoftServedWeb',
-        description: 'Speaker at "MVP Sprint"'
-    },
-    {
-        name: 'Milan P Sony',
-        image: MilanPSony,
-        designation: 'Growth Marketer and SEO Content Manager',
-        organization: 'Final Apps',
-        description: 'Speaker on "Digital Marketing & Growth Hacking"'
-    },
-    {
-        name: 'Nikhil Kilivayil',
-        image: NikhilKilivayil,
-        designation: 'Founder',
-        organization: 'Brototype',
-        description: 'Guest'
-    },
-    {
-        name: 'Nitheesh Kurian',
-        image: NitheeshKurian,
-        designation: 'Nodal Officer',
-        organization: 'RSET IEDC',
-        description: 'Speaker on "Finding & Validating a Startup Idea"'
-    },
-    {
-        name: 'Saurabh Wani',
-        image: SaurabhWani,
-        designation: 'Founding Growth Marketer',
-        organization: "Trupeer.ai, Ex Notion (YC S21)",
-        description: 'Speaker on "Leveraging social media and mastering content strategy"'
-    },
-    {
-        name: "Anvar T K",
-        image: AnvarTK,
-        designation: "Founder, CEO",
-        organization: "CookieYes & WebToffee",
-        description: 'Speaker on "How to find your first 100 customers"',
-    },
-    {
-        name: 'Eric Villa',
-        image: EricVilla,
-        designation: 'Head of Content Strategy',
-        organization: 'MKBHD',
-        description: 'Speaker on "Building an Engaging Online Presence: Content Strategy for Startups"'
-    },
-    {
-        name: 'Yasir M',
-        image: YasirM,
-        designation: 'Software Architect',
-        organization: 'Mercedes Benz',
-        description: 'Speaker on "Building Scalable Startups: Architecture Decisions & Pitfalls to Avoid"'
-    },
-    {
-        name: 'Vinod Balan',
-        image: VinodBalan,
-        designation: 'Founder',
-        organization: 'VOYE HOMES',
-        description: 'Mentor'
-    },
-    {
-        name: 'CS Manu Francis',
-        image: CSManuFrancis,
-        designation: 'Startup Growth Consultant',
-        organization: 'GHC Growth Lab',
-        description: 'Mentor'
-    },
-    {
-        name: 'Rabin Baby',
-        image: RabinBaby,
-        designation: 'Founder',
-        organization: 'Taxhero',
-        description: 'Mentor'
-    },
-    {
-        name: 'Jude Mathew',
-        image: JudeMathew,
-        designation: 'CTO & Founder',
-        organization: 'SmartCard AI',
-        description: 'Mentor'
-    },
-    {
-        name: 'Piyush Aggarwal',
-        image: PiyushAggarwal,
-        designation: 'Product Manager',
-        organization: 'Zepto',
-        description: 'Mentor'
-    },
-    {
-        name: 'Felix Josemon',
-        image: FelixJosemon,
-        designation: 'Founder',
-        organization: 'KPH, Final Apps, AB Final',
-        description: 'Guest'
-    }
+  
+    
 ];
